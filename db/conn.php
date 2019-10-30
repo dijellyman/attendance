c7c592a0c645415b7de8187bc01f7fea6d13e425
Dijellyman <rolest@live.com>
2019-10-30 07:09:32 -0500

    Initial commit

---
+++ db/conn.php
@@ -0,0 +1,34 @@
+<?php 
+
+    //Development connection
+   // $host = '127.0.0.1';
+   // $db = 'attendance_db';
+  //  $user = 'root';
+  //  $pass = '';
+  //  $charset = 'utf8mb4';
+
+   //Remote database connection
+    $host = 'remotemysql.com';
+    $db = 'SXGqm1jbQO';
+    $user = 'SXGqm1jbQO';
+    $pass = '3io8GxP2Av';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        throw new PDOException($e->getMessage());
+    }
+
+    require_once 'crud.php';
+    require_once 'user.php';
+    $crud = new crud($pdo);
+    $user = new user($pdo);
+
+    $user->insertUser("admin","password");
+   
+?>
---
+++ includes/successmessage.php
@@ -0,0 +1,3 @@
+<div class="alert alert-success" role="alert">
+    You have been registered for Cannex 2019
+</div>
